eeb9b9ae525b08696535ab4e4192c1b13e4340c2
Hector Velazquez <havg.1997@hotmail.com>
2017-12-02 16:10:46 -0600

    Proximidad
    Aplicacion para obtener paqueteria de Beacon

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ChangeDetectorRef } from "@angular/core";
+import { NavController, Platform } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  beaconData: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform,
+    private change: ChangeDetectorRef
+  ) {}
 
+  scanBeacon() {
+    this.platform.ready().then(() => {
+      evothings.eddystone.startScan(
+        data => {
+          this.beaconData = data;
+          console.log(this.beaconData);
+          setTimeout(() => {
+            this.change.detectChanges();
+          }, 1000);
+        },
+        error => console.error(error)
+      );
+    });
   }
-
 }
